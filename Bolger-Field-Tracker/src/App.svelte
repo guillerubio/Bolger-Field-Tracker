6d3133f60daf44799097db71ed42ef72c8483980
guillerubio <92153477+guillerubio@users.noreply.github.com>
2025-02-15 17:55:38 -0800

    Migrate to Vite

--- Bolger-Field-Tracker/src/App.svelte
+++ Bolger-Field-Tracker/src/App.svelte
@@ -1,30 +1,30 @@
 <script>
-	export let name;
-	import Map from "./Map.svelte";
+  export let name;
+  import Map from "./Map.svelte";
 </script>
 
 <main>
-	<h1>Haras de la Gesse Field Mapping</h1>
-	<Map />
+  <h1>Haras de la Gesse Field Mapping</h1>
+  <Map />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		font-size: 2em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    font-size: 2em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
