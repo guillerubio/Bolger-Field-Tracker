5f1794876c0e3a024eaf2b87a63256050cac0de1
guillerubio <92153477+guillerubio@users.noreply.github.com>
2025-02-15 18:19:01 -0800

    Updates

--- Bolger-Field-Tracker/svelte.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-export default defineConfig({
-  plugins: [svelte({ preprocess: vitePreprocess() })],
-  base: '/Bolger-Field-Tracker/' // Replace with your GitHub repo name
-});
